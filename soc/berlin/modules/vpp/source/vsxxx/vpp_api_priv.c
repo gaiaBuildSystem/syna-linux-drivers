a3fe079c6844c152dae9e778276a509717509680
Manojgouda Rabbanagoudra (CW) <manojgouda.rabbanagoudra@synaptics.com>
2025-09-04 06:11:16 +0000

    soc:berlin:vpp:Allow resolution to be set from dts
    - default resolution on dts changed to 4k30
    - If the sink cannot support dts configured resolution,
      highest supported sink resolution will be set
    
    JIRA:VSSDK-28375, CASE-1166
    
    Ported CL#263333
    
    Change-Id: Ib07219797268748c0727159f81d3425b71c1478a
    Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/273623
    Reviewed-by: Manojgouda Rabbanagoudra <Manojgouda.Rabbanagoudra@synaptics.com>
    Reviewed-by: Shanmugam Ramachandran <Shanmugam.Ramachandran@synaptics.com>

--- soc/berlin/modules/vpp/source/vsxxx/vpp_api_priv.c
+++ soc/berlin/modules/vpp/source/vsxxx/vpp_api_priv.c
@@ -306,17 +306,21 @@ static int VPP_Init_Recovery_vpp_ta(VPP_MEM_LIST *vpp_shm_list,
 			if (sinkCaps & ((1<<VPP_HDMI_SINKCAP_BITMASK_FULL4K)|(1<<VPP_HDMI_SINKCAP_BITMASK_4K30))) {
 				dispParams.uiResId = (sinkCaps & (1<<VPP_HDMI_SINKCAP_BITMASK_PREF50FPS)) ?
 					HDMI_MAX_RES_ENABLED_50_25:HDMI_MAX_RES_ENABLED_60_30;
-			} else if ((dispParams.uiResId == RES_1080P60) && (sinkCaps & (1<<VPP_HDMI_SINKCAP_BITMASK_FHD))) {
+			} else if (sinkCaps & (1<<VPP_HDMI_SINKCAP_BITMASK_FHD)) {
 				dispParams.uiResId = (sinkCaps & (1<<VPP_HDMI_SINKCAP_BITMASK_PREF50FPS)) ?
 					RES_1080P50:RES_1080P60;
 			} else {
 				dispParams.uiResId = (sinkCaps & (1<<VPP_HDMI_SINKCAP_BITMASK_PREF50FPS)) ?
 					RES_720P50:RES_720P60;
 			}
+			/*Set the resolution as set in dts if Sink supports it
+			else set maximium resolution supported by sink*/
+			dispParams.uiResId = vpp_config_param.disp1_res_id <= dispParams.uiResId ?
+				vpp_config_param.disp1_res_id : dispParams.uiResId;
 		}
-		pr_info("hdmi bootup to resId:%d colorfmt:%d bidepth:%d sinkcaps:%d\n",
+		pr_info("hdmi bootup to resId:%d colorfmt:%d bidepth:%d sinkcaps:%d dts-resId:%d\n",
 			dispParams.uiResId,dispParams.uiColorFmt,dispParams.uiBitDepth,
-			(res)?-1:sinkCaps);
+			(res)?-1:sinkCaps, vpp_config_param.disp1_res_id);
 	}
 
 	//Set the display resolution
