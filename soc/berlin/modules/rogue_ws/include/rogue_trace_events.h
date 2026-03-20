2eb9126dfe1e5039c8019fb2cbf06f7aeafe1fa9
Perisetti SaiRamMohanRao <perisetti.sairammohanrao@synaptics.com>
2026-03-20 15:00:55 +0530

    gpu: build pvr_gputrace and trace_events when trace is enabled
    CASE-3333
    Change-Id: I000fdbb8ef28cbffebe894dc74b9e4d7ed1ec86b
    Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/291678
    Reviewed-by: Perisetti SaiRamMohanRao <Perisetti.SaiRamMohanRao@synaptics.com>
    Reviewed-by: Bilu Thomas Augustine <Bilu.ThomasAugustine@synaptics.com>
    Reviewed-by: Kuldip Gor <Kuldip.Gor@synaptics.com>
    Reviewed-by: Jisheng Zhang <Jisheng.Zhang@synaptics.com>
    Reviewed-by: Kaicheng Xie <Kaicheng.Xie@synaptics.com>

--- soc/berlin/modules/rogue_ws/include/rogue_trace_events.h
+++ soc/berlin/modules/rogue_ws/include/rogue_trace_events.h
@@ -95,9 +95,9 @@ TRACE_EVENT_FN(rogue_fence_update,
 	),
 
 	TP_fast_assign(
-		__assign_str(comm, comm);
-		__assign_str(cmd, cmd);
-		__assign_str(dm, dm);
+		pvr_assign_str(comm, comm);
+		pvr_assign_str(cmd, cmd);
+		pvr_assign_str(dm, dm);
 		__entry->gpu_id = gpu_id;
 		__entry->ctx_id = ctx_id;
 		__entry->offset = offset;
@@ -146,9 +146,9 @@ TRACE_EVENT_FN(rogue_fence_check,
 	),
 
 	TP_fast_assign(
-		__assign_str(comm, comm);
-		__assign_str(cmd, cmd);
-		__assign_str(dm, dm);
+		pvr_assign_str(comm, comm);
+		pvr_assign_str(cmd, cmd);
+		pvr_assign_str(dm, dm);
 		__entry->gpu_id = gpu_id;
 		__entry->ctx_id = ctx_id;
 		__entry->offset = offset;
@@ -191,7 +191,7 @@ TRACE_EVENT(rogue_job_enqueue,
 		__entry->ctx_id = ctx_id;
 		__entry->int_id = int_id;
 		__entry->ext_id = ext_id;
-		__assign_str(kick_type, kick_type);
+		pvr_assign_str(kick_type, kick_type);
 	),
 
 	TP_printk("gpu=%lu, ctx_id=%lu int_id=%lu ext_id=%lu kick_type=%s",
@@ -223,7 +223,7 @@ TRACE_EVENT(rogue_sched_switch,
 	),
 
 	TP_fast_assign(
-		__assign_str(work_type, work_type);
+		pvr_assign_str(work_type, work_type);
 		__entry->switch_type = switch_type;
 		__entry->timestamp = timestamp;
 		__entry->gpu_id = gpu_id;
@@ -264,8 +264,8 @@ TRACE_EVENT(rogue_create_fw_context,
 	),
 
 	TP_fast_assign(
-		__assign_str(comm, comm);
-		__assign_str(dm, dm);
+		pvr_assign_str(comm, comm);
+		pvr_assign_str(dm, dm);
 		__entry->gpu_id = gpu_id;
 		__entry->ctx_id = ctx_id;
 	),
@@ -564,7 +564,7 @@ TRACE_EVENT_FN(rogue_firmware_activity,
 	TP_fast_assign(
 		__entry->timestamp = timestamp;
 		__entry->gpu_id = gpu_id;
-		__assign_str(task, task);
+		pvr_assign_str(task, task);
 		__entry->fw_event = fw_event;
 	),
 
